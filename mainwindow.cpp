6b39ce2da49ae6de206397776c1f75b02d8bef92
timothyvarghese <timothyvarghese@g.ucla.edu>
2018-03-13 23:00:44 -0700

    added final scores

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -35,13 +35,13 @@ MainWindow::MainWindow(QWidget *parent) :
 
     schemaa->setChecked(true);
 
+// This is where the midterm 1 code starts
+    QLabel* midlabel= new QLabel("Midterm 1"); // Create a label for midterm 1
 
-    QLabel* midlabel= new QLabel("Midterm 1");
+    QSpinBox *midsb = new QSpinBox(this); // Create a spinbox for midterm 1
+    midsb->setRange(0,100); // Set the range of the Spin Box from 0 to 100
 
-    QSpinBox *midsb = new QSpinBox(this);
-    midsb->setRange(0,100);
-
-    midslider = new QSlider(Qt::Horizontal);
+    midslider = new QSlider(Qt::Horizontal); // Create a new Slider for midterm 2
     // This connects a signal from the midsb to a slot function on mid slider and vice versa
     QObject::connect(midsb, SIGNAL(valueChanged(int)),midslider, SLOT(setValue(int)));
     QObject::connect(midslider, SIGNAL(valueChanged(int)),midsb
@@ -50,6 +50,8 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QHBoxLayout *layoutmid = new QHBoxLayout();
 
+
+    // This is where midterm 2 code starts
     QLabel* midlabel2= new QLabel("Midterm 2");
 
     QSpinBox *midsb2 = new QSpinBox(this); // Create a new spinbox for Midterm2
@@ -64,6 +66,20 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QHBoxLayout *layoutmid2 = new QHBoxLayout(); // Create a new layout for midterm2
 
+    // This is where the final exam code starts
+        QLabel* finallabel= new QLabel("Final Exam"); // Create a label for final
+
+        QSpinBox *finalsb = new QSpinBox(this); // Create a spinbox for final
+        finalsb->setRange(0,100); // Set the range of the Spin Box from 0 to 100
+
+        QSlider *finalslider = new QSlider(Qt::Horizontal); // Create a new Slider for final
+        // This connects a signal from the midsb to a slot function on mid slider and vice versa
+        QObject::connect(finalsb, SIGNAL(valueChanged(int)),finalslider, SLOT(setValue(int)));
+        QObject::connect(finalslider, SIGNAL(valueChanged(int)),finalsb
+                         ,SLOT(setValue(int)));
+        QObject::connect(finalslider, SIGNAL(valueChanged(int)),this,SLOT(computeGrades()));
+
+        QHBoxLayout *layoutfinal = new QHBoxLayout();
 
     QVBoxLayout *vbox = new QVBoxLayout;
     vbox->addWidget(schemaa);
@@ -74,7 +90,7 @@ MainWindow::MainWindow(QWidget *parent) :
     setCentralWidget(window);
 
 
-    for (int i=0; i<nhws; i++)
+    for (int i=0; i<nhws; i++) // Add the homeworks sliders and spinboxes
     {
         spinBox[i] = new QSpinBox; // This creates a new SpinBox for hw[i]
         spinBox[i]->setRange(0,100); // This sets the range of spinBox 2
@@ -104,13 +120,19 @@ MainWindow::MainWindow(QWidget *parent) :
     layoutmid->addWidget(midslider); // Add Midterm slider to the layout of Midterm 1
     layoutmid->addWidget(midsb);       // Add Midterm spinbox to the layout of Midterm 2
 
+
     layoutmid2->addWidget(midlabel2); // Add midterm 2 label to the layout of midterm 2
     layoutmid2->addWidget(midslider2); // Add midterm 2 slider to the
     layoutmid2->addWidget(midsb2);
 
+    layoutfinal->addWidget(finallabel); // Add final label to the layout of final
+    layoutfinal->addWidget(finalslider); // Add final slider to the layout
+    layoutfinal->addWidget(finalsb);
+
 
     mainLayout->addLayout(layoutmid);// Add Midterm1 Layout to the main layout
     mainLayout->addLayout(layoutmid2); // Add Midterm 2 Layout to the main layout
+    mainLayout->addLayout(layoutfinal); // Add final to the main layout
     mainLayout->addWidget(groupBox);
 
     mainLayout->addWidget(overallScore);
